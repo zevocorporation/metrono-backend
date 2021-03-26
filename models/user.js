47c6f424630330e645a4fc8c1bec938b4807a7e1
Jaisurya07 <jaisuryanarayanan141999@gmail.com>
2021-03-26 08:42:06 +0530

    Minimal Auth done!

--- models/user.js
+++ models/user.js
@@ -1,48 +1,48 @@
 const { Schema, model } = require('mongoose');
-const {
-  schema,
-} = require('../../../metrono-end-user-app/metrono-backend/models/user');
 
-const userSchema = new Schema({
-  firstname: {
-    type: String,
-    trim: true,
-  },
-  lastname: {
-    type: String,
-    trim: true,
-  },
-  username: {
-    type: String,
-    trim: true,
-    index: {
-      unique: true,
-      partialFilterExpression: { username: { $type: 'string' } },
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      trim: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      index: {
+        unique: true,
+        partialFilterExpression: { username: { $type: 'string' } },
+      },
+    },
+    mobile: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      index: {
+        unique: true,
+        partialFilterExpression: { email: { $type: 'string' } },
+      },
+    },
+    profilePicture: {
+      type: String,
+    },
+
+    address: {
+      type: String,
+    },
+    country: {
+      type: String,
     },
   },
-  mobile: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    index: {
-      unique: true,
-      partialFilterExpression: { email: { $type: 'string' } },
-    },
-  },
-  profilePicture: {
-    type: String,
-  },
-
-  address: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model('User', userSchema);
